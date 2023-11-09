26acb73661e13680b2af93a390a84582e930bc32
MrPericle <periclepergamo99@gmail.com>
2023-11-09 20:24:00 +0100

    DNS Server

--- LabReti/lab5/serverDNS.c
+++ LabReti/lab5/serverDNS.c
@@ -15,26 +15,35 @@ int main(int argc, char **argv)
     struct sockaddr_in servaddr;
     char buff[4096];
     time_t ticks;
+    socklen_t len;
+    struct sockaddr_in clientaddr;
+    char ipAddress[INET_ADDRSTRLEN];
 
-    listenfd = Socket(AF_INET,SOCK_STREAM,0);
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
+    // Inizializzazione diretta della struttura servaddr
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(13);
+    servaddr.sin_port = htons(1025);
 
-    if ( bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0 )
-    {
-        perror("bind");
-        exit(1);
-    }
+    Bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
     Listen(listenfd, 1024);
 
-    for ( ; ; ) {
-        connfd = Accept(listenfd, (struct sockaddr *) NULL, NULL); 
+    for (;;)
+    {
+        len = sizeof(clientaddr);
+        connfd = Accept(listenfd, (struct sockaddr *)&clientaddr, &len);
+
+        if (inet_ntop(AF_INET, &(clientaddr.sin_addr), ipAddress, sizeof(ipAddress)) == NULL)
+        {
+            perror("inet_ntop");
+            exit(1);
+        }
+
         ticks = time(NULL);
-        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-        FullWrite(connfd,buff,sizeof(buff));
+        snprintf(buff, sizeof(buff), "Serving client with IP: %s\n%.24s\r\n",ipAddress, ctime(&ticks));
+        FullWrite(connfd, buff, strlen(buff)); // Use strlen to get the actual length of the string
 
         close(connfd);
     }
